7bbca9db2ecbff7bc309f5f3aca073599d776ede
Seliom Gobeze <gobezes22@wlu.edu>
2020-12-17 06:50:18 -0600

    7 mins into Task 3 commit

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from foodtaskerapp import views
 
 urlpatterns = [
+# Web View - Admin
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+
+    # Web View - Restaurant
+    path('restaurant/sign_in', auth_views.LoginView.as_view(template_name='restaurant/sign_in.html'),
+        name='restaurant_sign_in'),
+    path('restaurant/sign_out', auth_views.LogoutView.as_view(next_page='/'),
+        name='restaurant_sign_out'),
+#path('restaurant/', views.restaurant_home, name = 'retsuarant=home')
 ]
